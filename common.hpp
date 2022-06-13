bbc305a27f98fb18e9b408acaa385d71187f59b2
danteking <danteking@aliyun.com>
2022-06-13 21:10:37 +0800

    header file

---
+++ common.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <cstring>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>

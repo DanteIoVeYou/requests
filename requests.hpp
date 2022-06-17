2ef48a3f02107b69412ea1bb7ee802c1a0cacee9
danteking <danteking@aliyun.com>
2022-06-17 23:54:11 +0800

    6 17

--- requests.hpp
+++ requests.hpp
@@ -4,12 +4,24 @@
 class requests
 {
 public:
-    requests() {}
-    ~requests() {}
-    void get() {}
+    requests()
+    {
+        _sock = Socket::Create();
+    }
+    ~requests()
+    {
+        close(_sock);
+    }
+    void get(std::string url, std::vector<std::pair<std::string, std::string>> data = {}, std::vector<std::pair<std::string, std::string>> header = {})
+    {
+        _data = std::move(data);
+        _header = std::move(header);
+        
+    }
     void post() {}
 
 private:
+    int _sock;
     std::string _url;                                         // url 地址
     std::vector<std::pair<std::string, std::string>> _data;   // url参数
     std::vector<std::pair<std::string, std::string>> _header; // 请求报头
